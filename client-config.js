e7f1df2eb1b5533f881e1fb49e76a98894bd887e
Dante Kelly <dante@dantekelly.com>
2021-02-10 21:18:03 -0800

    Modify default SEO

--- client-config.js
+++ client-config.js
@@ -4,9 +4,9 @@ module.exports = {
     dataset: process.env.GATSBY_SANITY_DATASET || 'development',
   },
   gatsby: {
-    siteTitle: 'Academy of Art Spring Show',
-    siteDescription: 'Academy of Art University Spring Show',
-    siteUrl: 'https://springshow.academyart.edu', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+    siteTitle: 'AAU Midpoint & Final Review Showcase',
+    siteDescription: 'Academy of Art University Midpoint & Final Review Show',
+    siteUrl: 'http://gradshowcase.academyart.edu/', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
     userTwitter: '@academy_of_art', // Change for Twitter Cards
     author: 'Academy of Art University', // Author for RSS author segment and SEO schema
     authorUrl: 'https://www.academyart.edu', // URL used for author and publisher schema, can be a social profile or other personal site
