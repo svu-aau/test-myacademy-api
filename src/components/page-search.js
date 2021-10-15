b20c7f08b710034752fe1e284d40cd07eba19cd8
aaron-chong <chongvelocity@gmail.com>
2021-10-15 14:55:38 -0400

    added searching component

---
+++ src/components/page-search.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react';
+import * as JsSearch from 'js-search';
+import { Link } from 'gatsby';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { highlightedMarkup, highlightedExtendedMarkup } from '../utils/search';
+import Pagination from './pagination';
+import { missingResults, searchResultStyle } from './page-search.module.css';
+
+const isBrowser = typeof window !== 'undefined';
+
+class PageSearch extends Component {
+  state = {
+    isLoading: true,
+    searchResults: [],
+    search: null,
+    isError: false,
+    termFrequency: true,
+    removeStopWords: false,
+    searchQuery: '',
+    selectedStrategy: 'All',
+    page: 1,
+    itemCount: 10,
+  };
+
+  /**
+   * React lifecycle method that will inject the data into the state.
+   */
+  async componentDidMount() {
+    this.rebuildIndex();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.state.search) {
+      if (this.props.searchTerm && this.props.searchTerm !== '') {
+        if (this.props.searchTerm !== prevProps.searchTerm) {
+          if (this.props.searchTerm !== this.state.searchQuery) {
+            this.searchData(this.props.searchTerm);
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * rebuilds the overall index based on the options
+   */
+  rebuildIndex = () => {
+    const { pages } = this.props;
+
+    const dataToSearch = new JsSearch.Search('id');
+
+    dataToSearch.tokenizer = new JsSearch.StopWordsTokenizer(new JsSearch.SimpleTokenizer());
+
+    /**
+     * defines an indexing strategy for the data
+     * read more about it here https://github.com/bvaughn/js-search#configuring-the-index-strategy
+     */
+    dataToSearch.indexStrategy = new JsSearch.AllSubstringsIndexStrategy();
+
+    /**
+     * defines the sanitizer for the search
+     * to prevent some of the words from being excluded
+     */
+    dataToSearch.sanitizer = new JsSearch.LowerCaseSanitizer();
+
+    dataToSearch.searchIndex = new JsSearch.TfIdfSearchIndex('id');
+
+    // sets the index attribute for the data
+    dataToSearch.addIndex('title');
+    // dataToSearch.addIndex('slug');
+    // dataToSearch.addIndex('synonyms');
+    dataToSearch.addIndex('body');
+    dataToSearch.addIndex('tags');
+
+    dataToSearch.addDocuments(pages); // adds the data to be searched
+
+    this.setState({ search: dataToSearch, isLoading: false }, () => this.searchData());
+  };
+
+  /**
+   * handles the input change and perform a search with js-search
+   * in which the results will be added to the state
+   */
+  searchData = (val = this.props.searchTerm) => {
+    const { search } = this.state;
+    const searchVal = decodeURIComponent(val);
+    const queryResult = search.search(searchVal);
+    this.setState({ searchQuery: searchVal, searchResults: queryResult });
+  };
+
+  changePage = (e, page) => {
+    this.setState({ page });
+    isBrowser && window.scrollTo(0, 0);
+  };
+
+  render() {
+    const { searchResults, searchQuery, isLoading, itemCount, page } = this.state;
+    const { pages, origin } = this.props;
+    const queryResults = searchQuery === '' ? pages : searchResults;
+
+    const maxPages = Math.ceil(queryResults.length / itemCount);
+
+    var begin = (page - 1) * itemCount;
+    var end = begin + itemCount;
+
+    const results = queryResults.slice(begin, end);
+
+    if (isLoading) {
+      return <CircularProgress />;
+    }
+
+    return (
+      <>
+        {results.length === 0 ? (
+          <>
+            <h1 className={missingResults}>{`No results found for "${searchQuery}"`}</h1>
+            <p style={{ textAlign: 'center' }}>Please try another keyword or phrase.</p>
+          </>
+        ) : (
+          <div className={searchResultStyle}>
+            <h1>{`Search Results for "${searchQuery}"`}</h1>
+            {results.map((result, idx) => {
+              let { slug } = result;
+              // console.log('slug: ', slug);
+              if (slug && !slug.startsWith('/')) {
+                slug = `/${slug}`;
+              }
+              return (
+                <div key={idx}>
+                  <h3>
+                    <Link to={slug}>{result.title}</Link>
+                  </h3>
+                  <p>
+                    {result.body[0] && (
+                      <>
+                        {highlightedMarkup(result.body[0], searchQuery)}
+                        <br />
+                      </>
+                    )}
+                    {highlightedExtendedMarkup(result.body, searchQuery)}
+                    <br />
+                    <Link to={slug}>{highlightedMarkup(slug, searchQuery, origin)}</Link>
+                  </p>
+                </div>
+              );
+            })}
+
+            {queryResults.length > 1 && <Pagination count={maxPages} onChange={this.changePage} />}
+          </div>
+        )}
+      </>
+    );
+  }
+}
+export default PageSearch;
---
+++ src/pages/search.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { navigate } from '@gatsbyjs/reach-router';
+
+import SEO from '../components/layout/seo';
+import Container from '../components/layout/container';
+import Section from '../components/sections/section';
+import PageSearch from '../components/page-search';
+
+const navigateBack = () => {
+  console.log('No query, navigating back');
+  navigate(-1);
+};
+
+const SearchTemplate = (props) => {
+  const { pageContext, location } = props;
+  const { pageData } = pageContext;
+  const { allPages } = pageData;
+  const { search, origin } = location;
+
+  const query = search.includes('?query=') && search.match(/query=([^&]+)/)[1];
+  !query && navigateBack();
+
+  return (
+    <>
+      <SEO title="Search Results" />
+      <div style={{ height: '98px' }} />
+      <Section>
+        <Container>
+          <PageSearch pages={allPages} searchTerm={query} origin={origin} />
+        </Container>
+      </Section>
+    </>
+  );
+};
+
+export default SearchTemplate;
