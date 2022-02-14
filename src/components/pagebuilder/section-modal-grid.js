83cf7c4c61fb106557edc091d6fde1913538bdb2
aaron-chong <chongvelocity@gmail.com>
2022-02-14 12:22:36 -0500

    fix last grid issue

--- src/components/pagebuilder/section-modal-grid.js
+++ src/components/pagebuilder/section-modal-grid.js
@@ -15,7 +15,6 @@ import {
   cardTitle,
   cardSubtitle,
   inActive,
-  gridCardContent,
   functionalGridTitle,
   contentClose,
 } from './section-modal-grid.module.css';
@@ -39,19 +38,35 @@ const SectionMediaGrid = ({ section }) => {
 
     if (window.matchMedia('(min-width: 600px) and (max-width: 959px)').matches) {
       const rational = (idx + 1) % 2;
+      const gridPosition = idx + (rational ? 2 - rational : rational);
       setModalState({
-        position: idx + (rational ? 2 - rational : rational),
+        position: section.grids[gridPosition] ? gridPosition : section.grids.length - 1,
         active: idx,
         open: true,
       });
     } else if (window.matchMedia('(min-width: 960px)').matches) {
       const rational = (idx + 1) % 3;
+      const gridPosition = idx + (rational ? 3 - rational : rational);
       setModalState({
-        position: idx + (rational ? 3 - rational : rational),
+        position: section.grids[gridPosition] ? gridPosition : section.grids.length - 1,
+        active: idx,
+        open: true,
+      });
+    } else {
+      setModalState({
+        position: idx,
         active: idx,
         open: true,
       });
     }
+  };
+
+  const closeGridContent = () => {
+    setModalState({
+      position: -1,
+      active: -1,
+      open: false,
+    });
   };
 
   useEffect(() => {
@@ -71,7 +86,7 @@ const SectionMediaGrid = ({ section }) => {
           <MUIGrid container spacing={3}>
             {section.grids.map((grid, idx) => (
               <>
-                <MUIGrid key={idx} item xs={1} sm={6} md={4} onClick={() => handleAction(idx)}>
+                <MUIGrid key={idx} item xs={12} sm={6} md={4} onClick={() => handleAction(idx)}>
                   <div>
                     <AspectRatio ratio={ratio}>
                       <img
@@ -85,7 +100,7 @@ const SectionMediaGrid = ({ section }) => {
                 </MUIGrid>
                 {idx === modalState.position && (
                   <MUIGrid item sm={12} style={{ position: 'relative' }}>
-                    <Button className={contentClose}>
+                    <Button className={contentClose} onClick={closeGridContent}>
                       <CloseIcon fontSize="large" />
                     </Button>
                     <Paper elevation={2} ref={gridContentTarget} />
